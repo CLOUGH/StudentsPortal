adcf5b37575f37ebb5d9b5e1e70ad03100bfbaf8
Warren Clough <clough.warren@gmail.com>
2012-09-16 14:58:56 -0500

    Update testSQLConnector and add a README file for the test scr

--- test/SQLConnector/MySQLConnectorTest.java
+++ test/SQLConnector/MySQLConnectorTest.java
@@ -12,9 +12,24 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class MySQLConnectorTest {
-
-	private Connection test_connection;
+    /**************************************************************************************
+	* Constants to run test
+	***************************************************************************************/
+	private static String DATABASE_ADDRESS = "mysql://localhost/HRDatabase";
+	private static String USERNAME = "root";
+	private static String PASSWORD = "gtx@6075";
+	private static String NOT_YET_IMPLEMENTENTED = "Test not yet implented";
 	
+	/**************************************************************************************
+	 * Instance variables used throughout the test
+	 ***************************************************************************************/
+	private Connection test_connection;
+	private RrsultSet test_results;,
+	
+	/**************************************************************************************
+	* Test1
+	* The system should be able to connect to a database
+	***************************************************************************************/
 	@Before
 	public void setUp() throws Exception {
 		test_connection = null;
@@ -23,8 +38,8 @@ public class MySQLConnectorTest {
 	@Test
 	public void testConnect() {
 		// Try open a connection to a database
-		try {			
-			test_connection = MySQLConnector.connect("mysql://localhost/HRDatabase","root", "gtx@6075");
+		try {
+			test_connection = MySQLConnector.connect(DATABASE_ADDRESS,USERNAME, PASSWORD);
 			System.out.println("The connection to the database was successful.");
 		}catch (ClassNotFoundException e) 
 		{
@@ -47,7 +62,53 @@ public class MySQLConnectorTest {
 		} catch (Exception e) {				
 			System.out.println("Error occured while closing the database.");
 			fail("Error occured while closing the database.");				
-		}			
+		}	
+		
+		
+		/************************************************************************************
+		* Test2
+		* System should be able to query the database
+		**************************************************************************************/
+		@Test
+		public void testQuery() {
+			fail(NOT_YET_IMPLEMENTENTED);
+		}
+		
+		
+		/*************************************************************************************
+		* Test3
+		* System should be able to get a column from the a selected set (query).
+		**************************************************************************************/
+		@Before
+		public void setUp(){
+			test_results = null;
+			try{
+				test_connection = MySQLConnector.connect(DATABASE_ADDRESS,USERNAME, PASSWORD);
+			}catch(Exception e){
+				fail("Could not setup connection to the database.");
+			}
+		}
+		@Test
+		public void testGetRecordData(){
+			fail(NOT_YET_IMPLEMENTENTED);
+		}
+		@After
+		public void destroy(){
+			try{
+				test_connection.close();
+		    }catch(Exception e){
+			    fail("Could not close the connection to the database.");
+		}
+	    /*************************************************************************************
+		* Test4
+		* System should be able to get name of tables in the database
+		**************************************************************************************/
+		@Test
+		public void testGetTableNames(){
+			int expected_number_tables=0;
+			String[] expected_names = new String[expected_number_tables];
+			fail(NOT_YET_IMPLEMENTENTED);
+		}
 	}
 
 }
